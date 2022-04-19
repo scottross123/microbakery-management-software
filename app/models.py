aa271ee0cab617a5a60cc53b63248bb9e1727c24
Scott Ross <scott.william.ross2@gmail.com>
2022-04-19 09:17:14 -0400

    added error.html and route to view tables

--- app/models.py
+++ app/models.py
@@ -1,11 +1,19 @@
 from . import db
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     f_name = db.Column(db.String(100), nullable=False)
     l_name = db.Column(db.String(100), nullable=False)
     phone_num = db.Column(db.String(12), nullable=False)
-    orders = db.relationship('Order', backref='customer', lazy=True)
+    customer = db.relationship('Order', backref='customer', lazy=True)
+
+    @hybrid_property
+    def name(self):
+        return self.f_name + " " + self.l_name
+
+    def __repr__(self):
+        return "<Customer(f_name='%s', l_name='%s', phone_num='%s')>" % (self.f_name, self.l_name, self.phone_num)
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -13,11 +21,18 @@ class Order(db.Model):
     pickup_time = db.Column(db.DateTime, nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
     lineitems = db.relationship('LineItem', backref='order', lazy=True)
+    orders = db.relationship('Customer', backref='customer', lazy=True)
+
+    def __repr__(self):
+        return "<Order(order_time='%s', pickup_time='%s', customer='%s')>" % (self.order_time, self.pickup_time, self.customer)
 
 class LineItem(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     order_id = db.Column(db.Integer, db.ForeignKey('order.id'), primary_key=True, nullable=False) 
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True, nullable=False)  
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), primary_key=True, nullable=False)
+
+    def __repr__(self):
+        return "<LineItem(quantity='%s', order='%s', product='%s')>" % (self.quantity, self.order, self.product)  
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -26,6 +41,9 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     lineitems = db.relationship('LineItem', backref='product', lazy=True)
     recipe = db.relationship('Recipe', backref='product', uselist=False)
+
+    def __repr__(self):
+        return "<Product(name='%s', description='%s', price='%s')>" % (self.name, self.description, self.price)
 
 recipe_ingredient = db.Table('recipe_ingredient', 
     db.Column('recipe_id', db.Integer, db.ForeignKey('recipe.id')),
@@ -40,16 +58,25 @@ class Recipe(db.Model):
     bulk_fermentation = db.Column(db.Integer, nullable=True)
     proof = db.Column(db.Integer, nullable=True)
     baking_time = db.Column(db.Integer, nullable=False)
-    ingredients = db.relationship('Ingredient', secondary=recipe_ingredient, backref='ingredients', lazy=True)
+    recipes = db.relationship('Ingredient', secondary=recipe_ingredient, backref='recipes', lazy=True)
     product_id = db.Column(db.Integer, db.ForeignKey('product.id'), unique=True)
+
+    def __repr__(self):
+        return "<Recipe(prep_time='%s', mix_time='%s', ddt='%s', bulk_fermentation='%s', proof='%s', baking_time='%s', product='%s', ingredients='%s')>" % (
+            self.prep_time, self.mix_time, self.ddt, self.bulk_fermentation, self.proof, self.baking_time, self.product, self.ingredients)
 
 class Ingredient(db.Model):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(500), nullable=False)
     cost = db.Column(db.Float, nullable=False)
+    ingredients = db.relationship('Recipe', secondary=recipe_ingredient, backref='ingredients', lazy=True)
 
     __mapper_args__ = {'polymorphic_identity': 'ingredient'}
+
+    def __repr__(self):
+        return "<Ingredient(name='%s', description='%s', cost='%s', recipes='%s')>" % (self.name, self.description, self.cost, self.recipes)
+    
 
 
 class Flour(Ingredient):
@@ -60,3 +87,6 @@ class Flour(Ingredient):
     malted = db.Column(db.Boolean, nullable=False)
 
     __mapper_args__ = {'polymorphic_identity': 'flour'}
+
+    def __repr__(self):
+        return "<Ingredient(name='%s', description='%s', cost='%s', ingredients='%s')>" % (self.name, self.description, self.cost, self.ingredients)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 import flask
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request
 import app
+from .models import Customer
 
 blueprint = Blueprint('blueprint', __name__, static_folder="static", template_folder='templates')
 
 @blueprint.route("/")
 def index():
     return flask.render_template("index.html")
+
+@blueprint.route("/<table>")
+def show_table(table):
+    
+    table = eval(table.capitalize())
+
+    try:
+        results = table.query.all()
+    except Exception as error:
+        return flask.render_template("error.html", error=error)
+
+    return flask.render_template("table.html", results=results)
+    
+    
