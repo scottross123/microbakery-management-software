05b5722e100a9d0e80011f3cfa4ecbdc2e964533
Scott Ross <scott.william.ross2@gmail.com>
2022-07-09 17:53:44 -0400

    fixed issue with react-router and spring boot, refreshing page woul result in 404 error from spring boot. added react controller to fix this

---
+++ src/main/java/com/example/scottross123/microbakery/controllers/ReactController.java
@@ -0,0 +1,15 @@
+package com.example.scottross123.microbakery.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
+}
